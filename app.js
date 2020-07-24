7f8d008de05b31d38f790db4dff0595f9dc151ea
nuesslerm <markus@choco.com>
2020-07-24 02:35:30 +0200

    created first app with feather 3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const feathers = require('@feathersjs/feathers');
-const app = feathers();
+// const app = feathers();
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+
 const faker = require('faker');
 
 class MessageService {
@@ -36,36 +39,75 @@ class MessageService {
   }
 }
 
-// register the message service on the feathers application
-app.use('messages', new MessageService());
+//----------------------------------------------------------------
 
-// log every time a new message has been created
+// creates an ExpressJS compatible Feathers application
+const app = express(feathers());
+
+// Parse HTTP JSON bodies
+app.use(express.json());
+// Parse URL-encoded params
+app.use(express.urlencoded({ extended: true }));
+// Host static files from the current folder
+app.use(express.static(__dirname));
+// Add REST API support
+app.configure(express.rest());
+// Configure Socket.io real-time APIs
+app.configure(socketio());
+// Register an in-memory messages service
+app.use('/messages', new MessageService());
+// Register a nicer error handler than the default Express one
+app.use(express.errorHandler());
+
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', (connection) => app.channel('everybody').join(connection));
+// Publish all events to the `everybody` channel
+app.publish((data) => app.channel('everybody'));
+
+// Start the server
 app
-  .service('messages')
-  .on('created', (message) =>
-    console.log('A new message has been created', message)
+  .listen(3030)
+  .on('listening', () =>
+    console.log('Feathers server listening on localhost:3030')
   );
 
-app.service('messages').on('removed', (removedMessage) => {
-  console.log('Removed message', removedMessage);
-  console.log('Remaining messages', app.service('messages').messages);
-});
+// Register the message service on the Feathers application
+// app.use('messages', new MessageService());
+
+// log every time a new message has been created
+// app
+//   .service('messages')
+//   .on('created', (message) =>
+//     console.log('A new message has been created', message)
+//   );
+
+// app.service('messages').on('removed', (removedMessage) => {
+//   console.log('Removed message', removedMessage);
+//   console.log('Remaining messages', app.service('messages').messages);
+// });
 
 // a function that creates new messages and then logs all existing messages
 const main = async (number) => {
   // create a new message on our message service
   for (let i = 0; i < number; i++) {
-    await app.service('messages').create({
+    app.service('messages').create({
       text: `what's up ${faker.name.firstName()}`,
     });
   }
 
-  await app.service('messages').remove(0);
+  app.service('messages').remove(0);
 
   // find all existing messages
-  const messages = await app.service('messages').find();
+  // const messages = await app.service('messages').find();
+  app.service('messages').find();
 
-  console.log('All messages', messages);
+  // console.log('All messages', messages);
 };
 
 main(100);
+
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('messages').create({
+  text: 'Hello world from the server',
+});
